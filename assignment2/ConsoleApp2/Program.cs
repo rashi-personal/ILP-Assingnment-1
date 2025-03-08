9080c4d5f876acb5f1fe99e9eb8289e16fdf66b5
rashi-personal <rashijain22062004@gmail.com>
2025-03-08 20:29:27 +0530

    Add files via upload

---
+++ assignment2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Logger
+    {
+        public virtual void log()
+        {
+            Console.WriteLine("This is logger");
+        }
+    }
+    public class DatabaseLogger : Logger
+    {
+        public override void log()
+        {
+            Console.WriteLine("this is a databaselogger");
+        }
+    }
+    public class fileLogger : Logger
+    {
+        public override void log()
+        {
+            Console.WriteLine("this is a filelogger");
+        }
+    }
+    public class cloudLogger : Logger
+    {
+        public override void log()
+        {
+            Console.WriteLine("this is a cloudlogger");
+        }
+    }
+
+
+
+}
---
+++ assignment2/ConsoleApp2/class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = GetLogger("file");
+            logger.log();
+            Logger logger1 = GetLogger("cloud");
+            logger1.log();
+            Logger logger2 = GetLogger("database");
+            logger2.log();
+        }
+        public static Logger GetLogger(string type)
+        {
+            if (type == "file")
+            {
+                return new fileLogger();
+            }
+            else if (type == "cloud")
+           
+            {
+                return new cloudLogger();
+            }
+            else
+            {
+                return new DatabaseLogger();
+            }
+        }
+    }
+}
+
